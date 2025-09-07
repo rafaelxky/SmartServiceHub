d2f506ec5cf7461e3c58c09c7eed2c6480028483
Rafael Penacho <rafapenacho@gmail.com>
2025-09-07 14:17:57 +0100

    Cahnged start.sh

--- start.sh
+++ start.sh
@@ -29,8 +29,8 @@ echo "Starting..."
 while true; do
   if curl -s "$MVN_URL" | grep -q "up"; then
     printf "\rThe app is ready to go! (in %ds)\n" "$timer"
-    printf "Get the backend endpoints at http://localhost:8080/home"
-    printf "Frontend URL http://localhost:5173/"
+    printf "Get the backend endpoints at http://localhost:8080/home \n"
+    printf "Frontend URL http://localhost:5173/ \n" 
 
     break
   else
