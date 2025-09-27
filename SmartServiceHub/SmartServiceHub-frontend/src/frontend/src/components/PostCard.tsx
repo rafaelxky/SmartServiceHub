698b0170f00ef8526e9b0c18afd8b1fe330114b6
Rafael Penacho <rafapenacho@gmail.com>
2025-09-27 23:27:13 +0100

    Tweaked card to use username and proper date

--- SmartServiceHub/SmartServiceHub-frontend/src/frontend/src/components/PostCard.tsx
+++ SmartServiceHub/SmartServiceHub-frontend/src/frontend/src/components/PostCard.tsx
@@ -1,19 +1,29 @@
 import { Card } from 'react-bootstrap';
 import type { ServicePost } from '../ts/api/models/ServicePost';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { userService } from '../pages/Context';
+import { useState, useEffect } from 'preact/hooks';
 
 interface Props {
   post: ServicePost;
 }
 
 const PostCard = ({ post }: Props) => {
+  const [username, setUsername] = useState<string>('author'); // default
+
+  useEffect(() => {
+    userService.getUserById(post.creatorId)
+      .then(user => setUsername(user.username))
+      .catch(err => console.error(err));
+  }, [post.creatorId]);
+
   return (
     <Card className="shadow-sm w-100">
       <Card.Body>
         <Card.Title>{post.title ?? "title"}</Card.Title>
         <Card.Text>{post.content ?? "content"}</Card.Text>
         <Card.Subtitle className="text-muted">
-          By {post.creatorId ?? "author"} on {post.timestamp ?? "date"}
+          By {username} on {new Date(post.timestamp).toLocaleDateString("de-DE") ?? "date"}
         </Card.Subtitle>
       </Card.Body>
     </Card>
